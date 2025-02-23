8bc8cc93b17f222e77907c8f4ab4f380287b50a7
Jonathan Berthias <jvberthias@gmail.com>
2025-02-23 21:26:20 +0100

    More tests for buildGenExprObj

--- tests/expr.pyi
+++ tests/expr.pyi
@@ -1,7 +1,19 @@
 from decimal import Decimal
+
 from typing_extensions import assert_type
+
 from pyscipopt import Expr
-from pyscipopt.scip import Constant, GenExpr, SumExpr, buildGenExprObj
+from pyscipopt.scip import (
+    Constant,
+    GenExpr,
+    PowExpr,
+    ProdExpr,
+    SumExpr,
+    UnaryExpr,
+    VarExpr,
+    Variable,
+    buildGenExprObj,
+)
 
 e: Expr
 g: GenExpr
@@ -11,10 +23,18 @@ class ToFloat:
     def __float__(self) -> float: ...
 
 assert_type(buildGenExprObj(1.0), Constant)
+assert_type(buildGenExprObj(expr=1.0), Constant)
+assert_type(buildGenExprObj(d), Constant)
 assert_type(buildGenExprObj(ToFloat()), Constant)
+
 assert_type(buildGenExprObj(e), SumExpr)
+assert_type(buildGenExprObj(Variable()), SumExpr)
+
 assert_type(buildGenExprObj(g), GenExpr)
-assert_type(buildGenExprObj(expr=e), SumExpr)
+assert_type(buildGenExprObj(PowExpr()), GenExpr)
+assert_type(buildGenExprObj(ProdExpr()), GenExpr)
+assert_type(buildGenExprObj(UnaryExpr()), GenExpr)
+assert_type(buildGenExprObj(VarExpr()), GenExpr)
 
 buildGenExprObj()  # pyright: ignore[reportCallIssue]
 buildGenExprObj(1j)  # pyright: ignore[reportArgumentType, reportCallIssue]
