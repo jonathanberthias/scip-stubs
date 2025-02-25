c83c05d9dd5c36582cca1cf71573672c80ec8125
Jonathan Berthias <jvberthias@gmail.com>
2025-02-25 22:27:51 +0100

    Move lp.pxi contents in same order as source

--- pyscipopt/scip.pyi
+++ pyscipopt/scip.pyi
@@ -219,6 +219,162 @@ def expr_to_nodes(expr): ...
 def value_to_array(val, nodes): ...
 def expr_to_array(expr, nodes): ...
 
+########
+# lp.pxi
+########
+
+class LP:
+    name: Incomplete
+    def __init__(self) -> None:
+        """
+        Keyword arguments:
+        name -- the name of the problem (default 'LP')
+        sense -- objective sense (default minimize)
+        """
+    def writeLP(self, filename):
+        """Writes LP to a file.
+
+        Keyword arguments:
+        filename -- the name of the file to be used
+        """
+    def readLP(self, filename):
+        """Reads LP from a file.
+
+        Keyword arguments:
+        filename -- the name of the file to be used
+        """
+    def infinity(self):
+        """Returns infinity value of the LP."""
+    def isInfinity(self, val):
+        """Checks if a given value is equal to the infinity value of the LP.
+
+        Keyword arguments:
+        val -- value that should be checked
+        """
+    def addCol(self, entries, obj=..., lb=..., ub=...):
+        """Adds a single column to the LP.
+
+        Keyword arguments:
+        entries -- list of tuples, each tuple consists of a row index and a coefficient
+        obj     -- objective coefficient (default 0.0)
+        lb      -- lower bound (default 0.0)
+        ub      -- upper bound (default infinity)
+        """
+    def addCols(self, entrieslist, objs=..., lbs=..., ubs=...):
+        """Adds multiple columns to the LP.
+
+        Keyword arguments:
+        entrieslist -- list containing lists of tuples, each tuple contains a coefficient and a row index
+        objs  -- objective coefficient (default 0.0)
+        lbs   -- lower bounds (default 0.0)
+        ubs   -- upper bounds (default infinity)
+        """
+    def delCols(self, firstcol, lastcol):
+        """Deletes a range of columns from the LP.
+
+        Keyword arguments:
+        firstcol -- first column to delete
+        lastcol  -- last column to delete
+        """
+    def addRow(self, entries, lhs=..., rhs=...):
+        """Adds a single row to the LP.
+
+        Keyword arguments:
+        entries -- list of tuples, each tuple contains a coefficient and a column index
+        lhs     -- left-hand side of the row (default 0.0)
+        rhs     -- right-hand side of the row (default infinity)
+        """
+    def addRows(self, entrieslist, lhss=..., rhss=...):
+        """Adds multiple rows to the LP.
+
+        Keyword arguments:
+        entrieslist -- list containing lists of tuples, each tuple contains a coefficient and a column index
+        lhss        -- left-hand side of the row (default 0.0)
+        rhss        -- right-hand side of the row (default infinity)
+        """
+    def delRows(self, firstrow, lastrow):
+        """Deletes a range of rows from the LP.
+
+        Keyword arguments:
+        firstrow -- first row to delete
+        lastrow  -- last row to delete
+        """
+    def getBounds(self, firstcol=..., lastcol=...):
+        """Returns all lower and upper bounds for a range of columns.
+
+        Keyword arguments:
+        firstcol -- first column (default 0)
+        lastcol  -- last column (default ncols - 1)
+        """
+    def getSides(self, firstrow=..., lastrow=...):
+        """Returns all left- and right-hand sides for a range of rows.
+
+        Keyword arguments:
+        firstrow -- first row (default 0)
+        lastrow  -- last row (default nrows - 1)
+        """
+    def chgObj(self, col, obj):
+        """Changes objective coefficient of a single column.
+
+        Keyword arguments:
+        col -- column to change
+        obj -- new objective coefficient
+        """
+    def chgCoef(self, row, col, newval):
+        """Changes a single coefficient in the LP.
+
+        Keyword arguments:
+        row -- row to change
+        col -- column to change
+        newval -- new coefficient
+        """
+    def chgBound(self, col, lb, ub):
+        """Changes the lower and upper bound of a single column.
+
+        Keyword arguments:
+        col -- column to change
+        lb  -- new lower bound
+        ub  -- new upper bound
+        """
+    def chgSide(self, row, lhs, rhs):
+        """Changes the left- and right-hand side of a single row.
+
+        Keyword arguments:
+        row -- row to change
+        lhs -- new left-hand side
+        rhs -- new right-hand side
+        """
+    def clear(self):
+        """Clears the whole LP."""
+    def nrows(self):
+        """Returns the number of rows."""
+    def ncols(self):
+        """Returns the number of columns."""
+    def solve(self, dual=...):
+        """Solves the current LP.
+
+        Keyword arguments:
+        dual -- use the dual or primal Simplex method (default: dual)
+        """
+    def getPrimal(self):
+        """Returns the primal solution of the last LP solve."""
+    def isPrimalFeasible(self):
+        """Returns True iff LP is proven to be primal feasible."""
+    def getDual(self):
+        """Returns the dual solution of the last LP solve."""
+    def isDualFeasible(self):
+        """Returns True iff LP is proven to be dual feasible."""
+    def getPrimalRay(self):
+        """Returns a primal ray if possible, None otherwise."""
+    def getDualRay(self):
+        """Returns a dual ray if possible, None otherwise."""
+    def getNIterations(self):
+        """Returns the number of LP iterations of the last LP solve."""
+    def getRedcost(self):
+        """Returns the reduced cost vector of the last LP solve."""
+    def getBasisInds(self):
+        """Returns the indices of the basic columns and rows; index i >= 0 corresponds to column i, index i < 0 to row -i-1"""
+
 ######
 # TODO
 ######
@@ -776,158 +932,6 @@ class Heur:
         """initializes primal heuristic"""
     def heurinitsol(self):
         """informs primal heuristic that the branch and bound process is being started"""
-
-class LP:
-    name: Incomplete
-    def __init__(self) -> None:
-        """
-        Keyword arguments:
-        name -- the name of the problem (default 'LP')
-        sense -- objective sense (default minimize)
-        """
-    def addCol(self, entries, obj=..., lb=..., ub=...):
-        """Adds a single column to the LP.
-
-        Keyword arguments:
-        entries -- list of tuples, each tuple consists of a row index and a coefficient
-        obj     -- objective coefficient (default 0.0)
-        lb      -- lower bound (default 0.0)
-        ub      -- upper bound (default infinity)
-        """
-    def addCols(self, entrieslist, objs=..., lbs=..., ubs=...):
-        """Adds multiple columns to the LP.
-
-        Keyword arguments:
-        entrieslist -- list containing lists of tuples, each tuple contains a coefficient and a row index
-        objs  -- objective coefficient (default 0.0)
-        lbs   -- lower bounds (default 0.0)
-        ubs   -- upper bounds (default infinity)
-        """
-    def addRow(self, entries, lhs=..., rhs=...):
-        """Adds a single row to the LP.
-
-        Keyword arguments:
-        entries -- list of tuples, each tuple contains a coefficient and a column index
-        lhs     -- left-hand side of the row (default 0.0)
-        rhs     -- right-hand side of the row (default infinity)
-        """
-    def addRows(self, entrieslist, lhss=..., rhss=...):
-        """Adds multiple rows to the LP.
-
-        Keyword arguments:
-        entrieslist -- list containing lists of tuples, each tuple contains a coefficient and a column index
-        lhss        -- left-hand side of the row (default 0.0)
-        rhss        -- right-hand side of the row (default infinity)
-        """
-    def chgBound(self, col, lb, ub):
-        """Changes the lower and upper bound of a single column.
-
-        Keyword arguments:
-        col -- column to change
-        lb  -- new lower bound
-        ub  -- new upper bound
-        """
-    def chgCoef(self, row, col, newval):
-        """Changes a single coefficient in the LP.
-
-        Keyword arguments:
-        row -- row to change
-        col -- column to change
-        newval -- new coefficient
-        """
-    def chgObj(self, col, obj):
-        """Changes objective coefficient of a single column.
-
-        Keyword arguments:
-        col -- column to change
-        obj -- new objective coefficient
-        """
-    def chgSide(self, row, lhs, rhs):
-        """Changes the left- and right-hand side of a single row.
-
-        Keyword arguments:
-        row -- row to change
-        lhs -- new left-hand side
-        rhs -- new right-hand side
-        """
-    def clear(self):
-        """Clears the whole LP."""
-    def delCols(self, firstcol, lastcol):
-        """Deletes a range of columns from the LP.
-
-        Keyword arguments:
-        firstcol -- first column to delete
-        lastcol  -- last column to delete
-        """
-    def delRows(self, firstrow, lastrow):
-        """Deletes a range of rows from the LP.
-
-        Keyword arguments:
-        firstrow -- first row to delete
-        lastrow  -- last row to delete
-        """
-    def getBasisInds(self):
-        """Returns the indices of the basic columns and rows; index i >= 0 corresponds to column i, index i < 0 to row -i-1"""
-    def getBounds(self, firstcol=..., lastcol=...):
-        """Returns all lower and upper bounds for a range of columns.
-
-        Keyword arguments:
-        firstcol -- first column (default 0)
-        lastcol  -- last column (default ncols - 1)
-        """
-    def getDual(self):
-        """Returns the dual solution of the last LP solve."""
-    def getDualRay(self):
-        """Returns a dual ray if possible, None otherwise."""
-    def getNIterations(self):
-        """Returns the number of LP iterations of the last LP solve."""
-    def getPrimal(self):
-        """Returns the primal solution of the last LP solve."""
-    def getPrimalRay(self):
-        """Returns a primal ray if possible, None otherwise."""
-    def getRedcost(self):
-        """Returns the reduced cost vector of the last LP solve."""
-    def getSides(self, firstrow=..., lastrow=...):
-        """Returns all left- and right-hand sides for a range of rows.
-
-        Keyword arguments:
-        firstrow -- first row (default 0)
-        lastrow  -- last row (default nrows - 1)
-        """
-    def infinity(self):
-        """Returns infinity value of the LP."""
-    def isDualFeasible(self):
-        """Returns True iff LP is proven to be dual feasible."""
-    def isInfinity(self, val):
-        """Checks if a given value is equal to the infinity value of the LP.
-
-        Keyword arguments:
-        val -- value that should be checked
-        """
-    def isPrimalFeasible(self):
-        """Returns True iff LP is proven to be primal feasible."""
-    def ncols(self):
-        """Returns the number of columns."""
-    def nrows(self):
-        """Returns the number of rows."""
-    def readLP(self, filename):
-        """Reads LP from a file.
-
-        Keyword arguments:
-        filename -- the name of the file to be used
-        """
-    def solve(self, dual=...):
-        """Solves the current LP.
-
-        Keyword arguments:
-        dual -- use the dual or primal Simplex method (default: dual)
-        """
-    def writeLP(self, filename):
-        """Writes LP to a file.
-
-        Keyword arguments:
-        filename -- the name of the file to be used
-        """
 
 class Model:
     data: Incomplete
