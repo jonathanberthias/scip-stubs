bc67572451d07d75edb503a51d8efa730a71da11
Jonathan Berthias <jvberthias@gmail.com>
2025-03-11 23:07:40 +0100

    Annotate multidict

--- pyscipopt/Multidict.pyi
+++ pyscipopt/Multidict.pyi
@@ -1,2 +1,9 @@
-def multidict(D):
-    """creates a multidictionary"""
+from collections.abc import Mapping, Sequence
+from typing import TypeVar
+
+_K = TypeVar("_K")
+_V = TypeVar("_V")
+
+def multidict(
+    D: Mapping[_K, _V] | Mapping[_K, Sequence[_V]],
+) -> list[list[_K] | dict[_K, _V]]: ...
