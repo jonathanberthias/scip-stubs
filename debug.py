b502a28404894b344a31ef7ec8364d987e53f164
Jonathan Berthias <jvberthias@gmail.com>
2025-03-16 22:58:49 +0100

    A few more helpers for debugging

--- debug.py
+++ debug.py
@@ -1,4 +1,7 @@
+from decimal import Decimal
+
 import pyscipopt as pyscipopt
+from pyscipopt import LP
 from pyscipopt.scip import Expr as Expr
 from pyscipopt.scip import GenExpr as GenExpr
 from pyscipopt.scip import Model, Term, Variable, buildGenExprObj
@@ -10,3 +13,7 @@ x: Variable = scip.addVar()
 y: Variable = scip.addVar()
 e = Expr({Term(x): 1})
 g = buildGenExprObj(e)
+d = Decimal("1.0")
+t = Term()
+
+lp = LP()
